de0f5adb1f776631b0ffb21fb73b34baf58446ed
BaSiczek <barbara.siczek@tooploox.com>
2018-07-02 14:08:10 +0200

    Cutting double __

--- test_BlackJack.py
+++ test_BlackJack.py
@@ -9,12 +9,12 @@ class TestBlackJack(unittest.TestCase):
         result = str(player)
         self.assertEqual(result, 'Bob your current balance is: 3000')
 
-    def test__boundary_bet_max_check_balance(self):
+    def test_boundary_bet_max_check_balance(self):
         player = BlackJack.Player('Bob', 3000)
         player.bet(3000)
         result = player.balance
         self.assertEqual(result, 0)
-    def test__boundary_insufficient_balance(self):
+    def test_boundary_insufficient_balance(self):
         player = BlackJack.Player('Bob', 3000)
         result = player.bet(3001)
         self.assertEqual(result, 'Insufficient balance')
